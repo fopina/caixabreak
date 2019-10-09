7e76978c7d088a076ec9e39d3963eea358326a87
Pina <fopina@gmail.com>
2019-10-09 01:39:59 +0100

    "Remember me" to persist login in localStorage #3

--- web/src/routes/home/index.js
+++ web/src/routes/home/index.js
@@ -6,7 +6,7 @@ import 'preact-material-components/List/style.css';
 import 'preact-material-components/Fab/style.css';
 import 'preact-material-components/LinearProgress/style.css';
 import style from './style';
-import { getData, updateData, getToken, setToken, refreshWithToken } from '../../utils/BreakService'
+import { getData, updateData, setToken, refreshWithSavedLogin, refreshWithToken } from '../../utils/BreakService'
 import { route } from 'preact-router';
 
 export default class Home extends Component {
@@ -16,25 +16,33 @@ export default class Home extends Component {
 
 	refresh = () => {
 		this.setState({ loading: true })
-		refreshWithToken(getToken()).then((data) => {
+		refreshWithToken().then((data) => {
 			updateData(data)
 			this.setState({ loading: false })
+			return
 		}).catch((error) => {
-			this.setState({ loading: false })
 			console.log(error)
-			if (error.response && error.response.data) {
-				if(error.response.data.Error === "not logged in") {
-					setToken(null)
-					route("/login/", true)
+			if ((error.response) && (error.response.status == 401)) {
+				refreshWithSavedLogin().then((data) => {
+					setToken(data.Token)
+					updateData(data)
+					this.setState({ loading: false })
 					return
-				}
-				else if(error.response.data.Error === "invalid token") {
-					setToken(null)
-					route("/login/", true)
-					return
-				}
+				}).catch((error) => {
+					console.log(error)
+					if ((error.response) && (error.response.status == 401)) {
+						setToken(null)
+						route("/login/", true)
+						return
+					} else {
+						this.setState({ loading: false })
+						alert('unexpected error')
+					}
+				})
+			} else {
+				this.setState({ loading: false })
+				alert('unexpected error')
 			}
-			alert('unexpected error')
 		});
 	}
 
--- web/src/routes/login/index.js
+++ web/src/routes/login/index.js
@@ -4,31 +4,40 @@ import TextField from 'preact-material-components/TextField';
 import linkState from 'linkstate';
 import LinearProgress from 'preact-material-components/LinearProgress';
 import Dialog from 'preact-material-components/Dialog';
+import Checkbox from 'preact-material-components/Checkbox'
+import Formfield from 'preact-material-components/FormField';
+import 'preact-material-components/Checkbox/style.css';
 import 'preact-material-components/Button/style.css';
 import 'preact-material-components/TextField/style.css';
 import 'preact-material-components/LinearProgress/style.css';
 import 'preact-material-components/Dialog/style.css';
 import style from './style';
-import {setToken, refreshWithLogin, updateData} from '../../utils/BreakService'
+import {setToken, saveLogin, removeLogin, refreshWithLogin, updateData} from '../../utils/BreakService'
 import { route } from 'preact-router';
 
 export default class Login extends Component {
 	state = {
 		loading: false,
 		username: '',
-		password: ''
+		password: '',
+		rememberMe: true,
 	};
 
 	login = () => {
+		console.log(this.state.rememberMe)
 		this.setState({ loading: true })
 		refreshWithLogin(this.state.username, this.state.password).then((data) => {
 			this.setState({ loading: false })
+			if (this.state.rememberMe) {
+				saveLogin(this.state.username, this.state.password)
+			}
 			setToken(data.Token)
 			updateData(data)
 			route("/", true)
 		}).catch((error) => {
 			this.setState({ loading: false })
 			if ((error.response) && (error.response.status == 401)) {
+				removeLogin()
 				this.scrollingDlg.MDComponent.show()
 			} else {
 				alert('unexpected error')
@@ -56,24 +65,28 @@ export default class Login extends Component {
 		return (
 			<div class={`${style.profile} page`}>
 				<h1>Login</h1>
-				<div>
-				<TextField label="Username" onKeyPress={this.keypress} value={state.username} onInput={linkState(this, 'username')}/>
-				</div>
-				<div>
-				<TextField label="Password" type="password" onKeyPress={this.keypress} value={state.password} onInput={linkState(this, 'password')}/>
-				</div>
+				<Formfield>
+					<TextField label="Username" onKeyPress={this.keypress} value={state.username} onInput={linkState(this, 'username')}/>
+				</Formfield>
+				<Formfield>
+					<TextField label="Password" type="password" onKeyPress={this.keypress} value={state.password} onInput={linkState(this, 'password')}/>
+				</Formfield>
+				<Formfield>
+					<Checkbox id="basic-checkbox" class={style.center_checkbox} checked={state.rememberMe} onChange={linkState(this, 'rememberMe')}/>
+					<label for="basic-checkbox">Remember me</label>
+				</Formfield>
 				<p>
 					<Button raised ripple onClick={() => this.login()}>Log in</Button>
 				</p>
 				<Dialog ref={scrollingDlg=>{this.scrollingDlg=scrollingDlg;}}>
-						<Dialog.Header>Error</Dialog.Header>
-						<Dialog.Body scrollable={true}>
-							Invalid login
-						</Dialog.Body>
-						<Dialog.Footer>
-							<Dialog.FooterButton accept={true}>OK</Dialog.FooterButton>
-						</Dialog.Footer>
-					</Dialog>
+					<Dialog.Header>Error</Dialog.Header>
+					<Dialog.Body scrollable={true}>
+						Invalid login
+					</Dialog.Body>
+					<Dialog.Footer>
+						<Dialog.FooterButton accept={true}>OK</Dialog.FooterButton>
+					</Dialog.Footer>
+				</Dialog>
 			</div>
 		);
 	}
